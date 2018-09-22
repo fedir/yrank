1b4131a0afc300f3dae3322c4059120de272f739
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-09-23 01:36:30 +0200

    Functions documentation

--- youtube/sorting.go
+++ youtube/sorting.go
@@ -2,18 +2,21 @@ package youtube
 
 import "sort"
 
+// SortByLikes sorts videos by it's likes
 func SortByLikes(vs []VideoStatistics) {
 	sort.Slice(vs[:], func(i, j int) bool {
 		return vs[i].LikeCount > vs[j].LikeCount
 	})
 }
 
+// SortByPositiveInterestingness sorts videos by it's positive (ignoring negative) interestingness
 func SortByPositiveInterestingness(vs []VideoStatistics) {
 	sort.Slice(vs[:], func(i, j int) bool {
 		return vs[i].PositiveInterestingness > vs[j].PositiveInterestingness
 	})
 }
 
+// SortByTotalInterestingness sorts videos by it's total interestingness
 func SortByTotalInterestingness(vs []VideoStatistics) {
 	sort.Slice(vs[:], func(i, j int) bool {
 		return vs[i].TotalInterestingness > vs[j].TotalInterestingness
