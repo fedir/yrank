08c6f3263538f4d8e4f0d737a4bdca641ca15acd
Fedir RYKHTIK <feodor.rykhtik@gmail.com>
2026-06-01 01:06:37 +0200

    refactor: convert playlist pagination from recursive to iterative

--- youtube/playlist.go
+++ youtube/playlist.go
@@ -8,37 +8,43 @@ import (
 )
 
 // PlaylistStatistics returns video statistics for every video in a playlist,
-// following pagination automatically.
+// following pagination iteratively.
 func PlaylistStatistics(playlistKey string, apiKey string, pageToken string, debug bool) []VideoStatistics {
-	url := "https://www.googleapis.com/youtube/v3/playlistItems?playlistId=" + playlistKey + "&maxResults=50&part=snippet%2CcontentDetails&key=" + apiKey
-	if pageToken != "" {
-		url += "&pageToken=" + pageToken
-	}
-	if debug {
-		fmt.Printf("Playlist URL: %s\n", url)
-	}
-
-	pl := fetchPlaylist(url)
-
-	dataChan := make(chan VideoStatistics, len(pl.Items))
-	var wg sync.WaitGroup
-	wg.Add(len(pl.Items))
-	for _, item := range pl.Items {
-		go videoStatistics(item.ContentDetails.VideoID, item.Snippet.Title, item.ContentDetails.VideoPublishedAt, apiKey, dataChan, &wg, debug)
-	}
-	wg.Wait()
-	close(dataChan)
-
 	var stats []VideoStatistics
-	for vs := range dataChan {
-		if vs.Title != "" {
-			stats = append(stats, vs)
+	token := pageToken
+
+	for {
+		url := "https://www.googleapis.com/youtube/v3/playlistItems?playlistId=" + playlistKey + "&maxResults=50&part=snippet%2CcontentDetails&key=" + apiKey
+		if token != "" {
+			url += "&pageToken=" + token
 		}
+		if debug {
+			fmt.Printf("Playlist URL: %s\n", url)
+		}
+
+		pl := fetchPlaylist(url)
+
+		dataChan := make(chan VideoStatistics, len(pl.Items))
+		var wg sync.WaitGroup
+		wg.Add(len(pl.Items))
+		for _, item := range pl.Items {
+			go videoStatistics(item.ContentDetails.VideoID, item.Snippet.Title, item.ContentDetails.VideoPublishedAt, apiKey, dataChan, &wg, debug)
+		}
+		wg.Wait()
+		close(dataChan)
+
+		for vs := range dataChan {
+			if vs.Title != "" {
+				stats = append(stats, vs)
+			}
+		}
+
+		if pl.NextPageToken == "" {
+			break
+		}
+		token = pl.NextPageToken
 	}
 
-	if pl.NextPageToken != "" {
-		stats = append(stats, PlaylistStatistics(playlistKey, apiKey, pl.NextPageToken, debug)...)
-	}
 	return stats
 }
 
